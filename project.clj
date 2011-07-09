b678cefc10c2d8232543efb3f7d040e74892a470
Wilfred Hughes <me@wilfred.me.uk>
2011-07-09 19:08:42 +0100

    fixing typo

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject shouter "0.0.1"
-  :description "Heroku's hello world with PosstreSQL"
+  :description "Heroku's hello world with PostreSQL"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [org.clojure/java.jdbc "0.0.3-SNAPSHOT"]
