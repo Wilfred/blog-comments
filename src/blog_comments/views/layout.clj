c35be83a56f0fada2582c86b6256fb6552b04434
Wilfred Hughes <me@wilfred.me.uk>
2011-07-10 22:42:32 +0100

    cleaning up template

--- src/blog_comments/views/layout.clj
+++ src/blog_comments/views/layout.clj
@@ -7,16 +7,10 @@
    (doctype :html5)
    [:head
     [:meta {:charset "utf-8"}]
-    [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
-    [:title title]
-    (include-css "/stylesheets/base.css"
-                 "/stylesheets/skeleton.css"
-                 "/stylesheets/screen.css")
-    (include-css "http://fonts.googleapis.com/css?family=Sigmar+One&v1")]
+    [:title title]]
    [:body
     [:div {:id "header"}
-     [:h1 {:class "container"} "SHOUTER"]]
+     [:h1 {:class "container"} "Blog comments admin"]]
     [:div {:id "content" :class "container"} body]]))
 
 (defn four-oh-four []
