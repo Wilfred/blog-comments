fe64c07996ece668ea100f11783a9b6e7c310719
Wilfred Hughes <me@wilfred.me.uk>
2011-07-20 00:15:40 +0100

    simplifying all comments view

--- src/blog_comments/views/comments.clj
+++ src/blog_comments/views/comments.clj
@@ -12,24 +12,22 @@
             (text-area "body")
             (submit-button "SHOUT!"))])
 
-(defn display-all-comments [approved-comments unapproved-comments]
-  (letfn [(display-comment [comment]
+(defn display-comment [comment]
             (list [:h2 {:class "comment"} (escape-html (:body comment))]
                   [:p "Created at: " (:created_at comment) " by " (:author comment)]
                   (if (not (:approved comment))
                     [:a {:href (str "/comments/" (:id comment) "/approve")} "Approve this comment"]
-                    [:a {:href (str "/comments/" (:id comment) "/unapprove")} "Unapprove this comment"])))]
-    (list [:p [:a {:href "/comments/create"} "Create new comment"]]
-          [:p [:a {:href "/posts/create"} "Create new post"]]
-          [:div
-           [:h2 "Unapproved comments"]
-           (map display-comment unapproved-comments)
-           [:h2 "Approved comments"]
-           (map display-comment approved-comments)])))
+                    [:a {:href (str "/comments/" (:id comment) "/unapprove")} "Unapprove this comment"])))
 
 (defn comments-all [approved-comments unapproved-comments]
   (layout/common "View all comments"
-                 (display-all-comments approved-comments unapproved-comments)))
+                 [:p [:a {:href "/comments/create"} "Create new comment"]]
+                 [:p [:a {:href "/posts/create"} "Create new post"]]
+                 [:div
+                  [:h2 "Unapproved comments"]
+                  (map display-comment unapproved-comments)
+                  [:h2 "Approved comments"]
+                  (map display-comment approved-comments)]))
 
 (defn comment-create []
   (layout/common "New comment"
