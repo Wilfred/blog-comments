22d92a1988c5afd61d30daca45ae47858072a701
Wilfred Hughes <me@wilfred.me.uk>
2011-07-19 22:51:13 +0100

    adding a post creation form

--- src/blog_comments/controllers/posts.clj
+++ src/blog_comments/controllers/posts.clj
@@ -1,10 +1,10 @@
 (ns blog-comments.controllers.posts
   (:use [compojure.core :only [defroutes GET]])
-  (:require [blog-comments.views.layout :as layout]))
+  (:require [blog-comments.views.layout :as layout]
+            [blog-comments.views.posts :as view]))
 
 (defn post-create []
-  (layout/common "Hello world"
-                 [:h2 "Hi there"]))
+  (view/post-create))
 
 (defroutes routes
   (GET "/posts/create" [] (post-create)))
---
+++ src/blog_comments/views/posts.clj
@@ -0,0 +1,18 @@
+(ns blog-comments.views.posts
+  (:use [hiccup.core :only [html escape-html]]
+        [hiccup.page-helpers :only [doctype]]
+        [hiccup.form-helpers :only [form-to label text-area text-field submit-button]])
+  (:require [blog-comments.views.layout :as layout]))
+
+(defn post-form []
+  [:div
+   (form-to [:post "/post/create"]
+            (label "name" "Post name")
+            (text-field "name")
+            (submit-button "Create post"))])
+
+
+(defn post-create []
+  (layout/common "Create a new post"
+                 [:h2 "Create new post"]
+                 (post-form)))
